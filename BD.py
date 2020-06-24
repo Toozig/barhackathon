936ec6ec65690475578773084d0a3f0a2bf8e632
galtoledano <>
2020-06-24 15:45:32 +0300

    BD waiting to force function

--- BD.py
+++ BD.py
@@ -1,29 +1,61 @@
 import argparse
-# import seaborn as sbn
+import seaborn as sbn
 import matplotlib.pyplot as plt
 
 import numpy as np
 
-DEFAULT_SHAPE = (5, 5)
+# DEFAULT_SHAPE = (5, 5)
 DT = 0.1
 BD_FACTOR = 6
-FORCE_VEC = np.asarray([-0.1, -0.5])
+# FORCE_VEC = np.asarray([-0.1, -0.5])
 UPPER_BOUND = 4.5
 LOWER_BOUND = -0.5
+MAX_DEPTH = 5
 
 
 class BD:
-    def __init__(self, n, kt, dt, d, x0, force_f):
+    def __init__(self, n, kt, dt, d, x0, force_f, dim):
+        """
+        :param n: iteration number
+        :param kt: kT
+        :param dt: time interval length
+        :param d: D
+        :param x0: initial state
+        :param force_f: function that calculate the forces that work on the proteins
+        :param dim: the dimension of the rectangle that we looking at
+        """
         self.__n = n
         self.__kt = kt
         self.__dt = dt
         self.__d = d
         self.__x0 = x0
         self.__force_func = force_f
+        self.__dim = dim
         self.__r = 0
 
+    def __vec_in_rec(self, vec, x_0, y_0, z_0, x_1, y_1, z_1):
+        """
+        The function checks if the vactor values are in the rectangle
+        :param vec: the vector to check
+        :param x_0: lower x bound
+        :param y_0: lower y bound
+        :param z_0: lower z bound
+        :param x_1: upper x bound
+        :param y_1: upper y bound
+        :param z_1: upper z bound
+        :return:
+        """
+        for i in vec:
+            if self.__dim == 3:
+                if not ((x_0 <= vec[0] <= x_1) or (y_0 <= vec[1] <= y_1) or (z_0 <= vec[2] <= z_1)):
+                    return False
+            elif self.__dim == 2:
+                if not ((x_0 <= vec[0] <= x_1) or (y_0 <= vec[1] <= y_1)):
+                    return False
+        return True
 
-    def BD_algorithm():
+
+    def BD_algorithm(self):
         """
         The Brownian dynamic algorithm implementation,  as described in our paper.
         :param n: Number of iterations
@@ -36,41 +68,27 @@ class BD:
         # y = np.random.uniform(LOWER_BOUND, UPPER_BOUND)
         cur = self.__x0
         print(cur)
-        while i < range(self.__n):
+        i , j = 0, 0
+        while i < self.__n:
+            if j > MAX_DEPTH:
+                print("error, stack outside of the square")
+                return
             r = np.random.normal(0, 1, 2)
             # result[round(cur[0]), round(cur[1])] += 1
             addition = self.__dt * self.__force_func(cur) + r * np.sqrt(BD_FACTOR * self.__kt * self.__dt)
             temp = cur + addition
 
             # todo : try to write the function again
-            if LOWER_BOUND < temp[0] < UPPER_BOUND and LOWER_BOUND < temp[1] < UPPER_BOUND:
+            if self.__vec_in_rec(cur, LOWER_BOUND, LOWER_BOUND, 0, UPPER_BOUND, UPPER_BOUND, 0): #todo: decide the rectangle boundaries
                 cur = temp
                 result[i] = cur
                 i += 1
+                j = 0
             else:
                 temp -= addition
+                j += 1
 
         return result
-
-    def Brownian_dynamics(n, R, t, kT):
-        grid = np.zeros((5, 5))
-        x = np.random.uniform(-0.5, 4.5, 2)
-        force = np.array([-1, -0.5])
-        # cur_x = np.random.uniform(-0.5, 4.5)
-        # cur_y = np.random.uniform(-0.5, 4.5)
-        for i in range(n):
-            grid[int(round(x[0])), int(round(x[1]))] += 1
-            f_x = force @ x
-            r_vec = np.random.normal(size=2)
-            x = x + t * f_x + np.sqrt(6 * kT * t) * r_vec
-            # if x[0] < -0.5: x[0] = 2 + (-0.5 + x[0])
-            # if x[1] < -0.5: x[1] = 2 + (-0.5 + x[1])
-            # if x[0] > 4.5: x[0] = 2 + (4.5 - x[0])
-            # if x[1] > 4.5: x[1] = 2 + (4.5 - x[1])
-            if x[0] < -0.5: x[0] = -0.5
-            if x[1] < -0.5: x[1] = -0.5
-            if x[0] > 4.5: x[0] = 4.5
-            if x[1] > 4.5: x[1] = 4.5
 
 
 def get_cmdline_parser():
@@ -86,12 +104,12 @@ def get_cmdline_parser():
 
 if __name__ == '__main__':
     parser = get_cmdline_parser().parse_args()
-    plt.figure()
-    for i in [0.1, 1]:
-        result = BD_algorithm(1000000, i)
-        print(result)
-        sbn.heatmap(result / np.sum(result), annot=True, fmt=".2f", linewidths=.5)
-        plt.title("Configuration frequency heatmap for BD algorithm (kT = %.2f)" % i)
-        plt.legend("frequency of visits")
-        plt.show()
-        plt.savefig("BD_alg_kt_%.2f.png" % i)
+    # plt.figure()
+    # for i in [0.1, 1]:
+    #     result = BD_algorithm(1000000, i)
+    #     print(result)
+    #     sbn.heatmap(result / np.sum(result), annot=True, fmt=".2f", linewidths=.5)
+    #     plt.title("Configuration frequency heatmap for BD algorithm (kT = %.2f)" % i)
+    #     plt.legend("frequency of visits")
+    #     plt.show()
+    #     plt.savefig("BD_alg_kt_%.2f.png" % i)
